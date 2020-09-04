7338aba30a567898e241469addc6a580e7b268f7
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-05 04:39:06 +0530

    tutorial-09
    Covert string to integer.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,13 +10,9 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // tutorial-08
-            Console.Write("Enter your name: "); // Console.WriteLine adds "\n" at the end so w/o "\n" 
-            string usrName = Console.ReadLine(); // Big Revelation => used to get a input from user.
-
-            Console.Write("Enter your age: ");
-            string usrAge = Console.ReadLine();
-            Console.WriteLine("Hello " + usrName + ", great to know you are " + usrAge + " years old.");
+            // tutorial-09
+            int num = Convert.ToInt32("45"); // String to 32Bit number
+            Console.WriteLine(num + 6);
             Console.ReadLine(); // to make cmd stay on-screen
         }
     }
