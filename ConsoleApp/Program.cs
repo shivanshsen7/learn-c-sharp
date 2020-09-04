8b12b5c4d6f19a86500c63310047793a1bfdeec9
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-05 04:46:12 +0530

    tutorial-10
    Type casting

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,9 +10,15 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // tutorial-09
-            int num = Convert.ToInt32("45"); // String to 32Bit number
-            Console.WriteLine(num + 6);
+            // tutorial-10
+            Console.Write("Enter a number :");
+            double num1 = Convert.ToDouble(Console.ReadLine()) ; // String to 32Bit number
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            //// Try Uncomment this one.
+            //Console.WriteLine("Sum of " + num1 + " and " + num2 + " is " + num1 + num2); 
+            Console.WriteLine("Sum of " + num1+ " and " + num2 + " is " + (num1+num2));
+
             Console.ReadLine(); // to make cmd stay on-screen
         }
     }
