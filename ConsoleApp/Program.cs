9c0ce60cf1f4ace0021ed5bcf02dc8dcd16b07f2
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-05 12:14:12 +0530

    tutorial-13
    function in OOP is called method.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,28 +10,14 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // tutorial-12
-            int[] luckyNumbers = { 1, 2, 3, 5 };
-                   // indexing =>  0  1  2  3
-            Console.WriteLine(luckyNumbers[0]); // first element
-            luckyNumbers[0] = 11; // arrays are mutable
-            Console.WriteLine(luckyNumbers[0]); // first element
-
-            string[] usrNames = new string[5]; // 5 is limit of number of elements
-            usrNames[0] = "USER00";
-            usrNames[1] = "USER01";
-            usrNames[2] = "USER02";
-            usrNames[3] = "USER03";
-            usrNames[4] = "USER04";
-            Console.WriteLine(usrNames[0]);
-            Console.WriteLine(usrNames[1]);
-            Console.WriteLine(usrNames[2]);
-            Console.WriteLine(usrNames[3]);
-            Console.WriteLine(usrNames[4]);
-            // invalid assignment
-            //usrNames[5] = "USER05"; // string out of bounds
-            //Console.WriteLine(usrNames[5]);
+            // tutorial-13
+            SayHi(); // runs only when called in main
             Console.ReadLine(); // to make cmd stay on-screen
+        }
+
+        static void SayHi()
+        {
+            Console.WriteLine("Hi, User");
         }
     }
 }
