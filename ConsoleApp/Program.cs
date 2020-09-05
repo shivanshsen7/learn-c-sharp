83210137f0693f3497d6cedde7170aa570a020f6
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-05 12:56:37 +0530

    tutorial-21
    if else statments and boolean evaluations.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,14 +10,28 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // tutorial-20
-            int age; 
-            
+            // tutorial-21
+            int age;
+            bool isMale;
             Console.Write("Enter your Age: ");
             age = Convert.ToInt32(Console.ReadLine());
 
             if (age >= 18) {
                 isAdult();
+            }
+            Console.Write("If male enter 'y': ");
+            if ("y" == Console.ReadLine()) {
+                Console.WriteLine("You are male");
+            }
+            //Console.Write("If male enter 'y': ");
+            //isMale = "y" == Console.ReadLine();
+            //if (isMale)
+            //{
+            //    Console.WriteLine("You are male");
+            //}
+            else
+            {
+                Console.WriteLine("You are not male");
             }
 
             Console.ReadLine(); // to make cmd stay on-screen
