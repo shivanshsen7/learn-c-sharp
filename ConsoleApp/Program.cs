58305c76a3a511a0c4678f2495b04f58ba9a054e
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-05 12:04:44 +0530

    tutorial-12
    Arrays

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,21 +10,27 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // tutorial-11
-            string color, pluralNoun, celebrity;
+            // tutorial-12
+            int[] luckyNumbers = { 1, 2, 3, 5 };
+                   // indexing =>  0  1  2  3
+            Console.WriteLine(luckyNumbers[0]); // first element
+            luckyNumbers[0] = 11; // arrays are mutable
+            Console.WriteLine(luckyNumbers[0]); // first element
 
-            Console.Write("Enter a Color: ");
-            color = Console.ReadLine();
-            Console.Write("Enter a Plural Noun: ");
-            pluralNoun = Console.ReadLine(); 
-            Console.Write("Enter a  celebrity: ");
-            celebrity = Console.ReadLine();
-
-            Console.WriteLine("Roses are " + color + ".");
-            Console.WriteLine( pluralNoun + " are Blue.");
-            Console.WriteLine("I love " + celebrity + ".");
-
-
+            string[] usrNames = new string[5]; // 5 is limit of number of elements
+            usrNames[0] = "USER00";
+            usrNames[1] = "USER01";
+            usrNames[2] = "USER02";
+            usrNames[3] = "USER03";
+            usrNames[4] = "USER04";
+            Console.WriteLine(usrNames[0]);
+            Console.WriteLine(usrNames[1]);
+            Console.WriteLine(usrNames[2]);
+            Console.WriteLine(usrNames[3]);
+            Console.WriteLine(usrNames[4]);
+            // invalid assignment
+            //usrNames[5] = "USER05"; // string out of bounds
+            //Console.WriteLine(usrNames[5]);
             Console.ReadLine(); // to make cmd stay on-screen
         }
     }
