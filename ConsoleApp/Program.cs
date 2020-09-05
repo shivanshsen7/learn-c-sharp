970f94939b6d7fdfee2fe13a43560172bea4bf43
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-05 14:16:53 +0530

    task-01
    abuguity clearance

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp
             // task-01
             // Complete "reMax" method that accept two integer and 
             // return the greater integer of the two
+            // in case of equality return any of the two :)
             int num1 = 7, num2 = 77;
             int maxNum = reMax(num1, num2);
             Console.WriteLine("Max of " + num1 + " and "+ num2 + " is " + maxNum);
