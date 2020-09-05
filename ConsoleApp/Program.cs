02bfe382ea146495c005eb7e93dcd328f0ec38ec
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-05 13:42:41 +0530

    tutorial-24
    logical OR operator

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,27 +10,27 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // tutorial-23
+            // tutorial-24
           
             bool isMale = true, isTall = true;
 
             // DRY => Don't Repeat Yourself
-            isTallMale(isMale, isTall);
-            isTallMale(true, false);
+            isTallOrMale(isMale, isTall);
+            isTallOrMale(true, false);
 
             Console.ReadLine(); // to make cmd stay on-screen
         }
 
-        static void isTallMale(bool isMale, bool isTall)
+        static void isTallOrMale(bool isMale, bool isTall)
         {
-            if (isMale && isTall)
+            if (isMale || isTall)
             {
-                Console.WriteLine("You are a tall male");
+                Console.WriteLine("You are either tall or male, or both.");
             }
 
             else
             {
-                Console.WriteLine("You either not male or not tall, or both");
+                Console.WriteLine("You either neither male or nor tall.");
             }
         }
     }
