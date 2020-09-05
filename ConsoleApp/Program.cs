2cb0444df4e1e9bb3da983202c33ddc18e2f851e
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-05 21:05:32 +0530

    tutorial-30
    Guessing game While loop

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,49 +12,31 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //tutorial-29
-            int day = 7;
-            do
+            //tutorial-30
+            int guess, val = 65;
+            bool guessed = false;
+            while (!guessed)
             {
-                Console.WriteLine(GetDay(day));
-                ++day;  // or day++;
-            } while (day < 7);
-
-                Console.ReadLine(); // to make cmd stay on-screen
-        }
-        static string GetDay(int dayNum)
-        {
-            string dayName;
-
-            switch (dayNum)
-            {
-                case 0:
-                    dayName = "Monday";
-                    break;
-                case 1:
-                    dayName = "Tuesday";
-                    break;
-                case 2:
-                    dayName = "Wednesday";
-                    break;
-                case 3:
-                    dayName = "Thrusday";
-                    break;
-                case 4:
-                    dayName = "Friday";
-                    break;
-                case 5:
-                    dayName = "Saturday";
-                    break;
-                case 6:
-                    dayName = "Sunday";
-                    break;
-                default:
-                    dayName = "Invalid Day";
-                    break;
+                Console.Write("Guess the number: ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                if (guess < val)
+                {
+                    Console.WriteLine("Guess Higher");
+                }
+                else if (guess > val)
+                {
+                    Console.WriteLine("Guess Lower");
+                }
+                else
+                {
+                    guessed = true;
+                    Console.WriteLine("You Guessed right the number was " + guess + " .");
+                    
+                }
             }
-
-            return dayName;
-        } 
+           
+            Console.ReadLine(); // to make cmd stay on-screen
+        }
+        
     }
 }
