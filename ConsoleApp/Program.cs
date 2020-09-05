5fe006d99c407584b1fdc346ac5cc9246b1dc8b8
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-05 12:43:05 +0530

    tutorial-19
    Type conversion drag

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,24 +10,24 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            // tutorial-18 
+            // tutorial-19
             int num, numCube; 
           
             Console.Write("Enter a integer: ");
             num = Convert.ToInt32(Console.ReadLine());
             numCube = Cube(num);
-            // need to use so assigned to a variable
-            // while it will return nothing seems to happen unless wee use the return data
             Console.WriteLine(numCube);
             Console.ReadLine(); // to make cmd stay on-screen
         }
 
         static int Cube(int number) 
         {
-            return number * number * number;
+            // return Math.Pow(Convert.ToDouble(number), 3);// can not implict convert
+            return Convert.ToInt32(Math.Pow(Convert.ToDouble(number), 3));
             // can't use Math.Pow(number, 3)
             // as that requires doubles
             // wait can we do that?
+            // Oh Yeah!, we did it. But man what a drag!
 
         }
     }
