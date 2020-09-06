11c963fe37c184f5210bc2341f09662feb453efc
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-06 17:50:41 +0530

    tutorial-50
    Inheritance and Overloading

---
+++ ConsoleApp/Chef.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Chef
+    {
+        static protected int Salary = 5000;
+        static public void Dairy()
+        {
+            Console.WriteLine("Dairy Products");
+        }
+
+        
+    }
+    class VegChef : Chef
+    {
+        static public new int Salary = 5500;
+        static public void Veg()
+        {
+            Console.WriteLine("All Veg");
+        }
+    }
+    class NonVegChef:Chef
+    {
+
+        static public new int Salary = 4500;
+        static public void NonVeg()
+        {
+            Console.WriteLine("Non Veg");
+        }
+    }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,44 +13,17 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //tutorial-49
+            //tutorial-50
+            Chef.Dairy();
+            // Console.WriteLine(Chef.Salary);
+            VegChef.Dairy();
+            VegChef.Veg();
+            Console.WriteLine(VegChef.Salary);
 
-            Console.WriteLine(Book.Count);
-            Book HPOne = new Book(
-                title_:"Harry Potter and the Philosopher's Stone", 
-                author_:"J.K. Rowling",
-                pages_:223); // Named Arguments
+            NonVegChef.Dairy();
+            NonVegChef.NonVeg();
+            Console.WriteLine(NonVegChef.Salary);
 
-            Console.WriteLine(Book.Count);
-            Book HPTwo = new Book(
-                "Harry Potter and the Chamber of Secrets",
-                "J.K. Rowling",
-                251
-            ); // Positional Arguments
-
-
-            Console.WriteLine(Book.Count);
-            Book HPThree = new Book(
-                "Harry Potter and the Prisoner of Azkaban",
-                "J.K. Rowling"
-                
-            );
-
-            // HPThree.Pages = 100001;
-            // HPThree.Pages = 0;
-            HPThree.Pages = 317;
-            Console.WriteLine(Book.Count);
-            Book HPFour = new Book(pages_:636)
-            {
-                title = "Harry Potter and the Goblet of Fire",
-                author = "J.K. Rowling"
-                
-            };
-
-            Console.WriteLine(Book.Count);
-            Console.Write("sine(pi/2) = ");
-
-            Console.WriteLine(Math.Sin((Math.PI) / 2));
             Console.ReadLine(); // to make cmd stay on-screen
         }
            
