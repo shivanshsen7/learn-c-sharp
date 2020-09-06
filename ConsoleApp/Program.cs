dca20081cac572a05bb875fe5a37c7b79b3816c6
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-06 12:17:32 +0530

    tutorial-36
    Error handling intro Try-catch-finally

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,14 +12,25 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //tutorial-35
+            //tutorial-36
             double num1, num2, result;
-            Console.Write("Num1: ");
-            num1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Num2: ");
-            num2 = Convert.ToDouble(Console.ReadLine());
-            result = num1 / num2;
-            Console.WriteLine($"{num1} / {num2} == {result}");
+            try
+            {
+                Console.Write("Num1: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Num2: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+                result = num1 / num2;
+                Console.WriteLine($"{num1} / {num2} == {result}");
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            finally
+            {
+                Console.WriteLine("Have a nice day!");
+            }
             Console.ReadLine(); // to make cmd stay on-screen
         }
            
