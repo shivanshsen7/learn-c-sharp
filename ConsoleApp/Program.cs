df62697dfdfb179ae1ba0a80d4c1b495c26f0482
shivanshsen7 <shivanshsen7@gmail.com>
2020-09-06 12:47:19 +0530

    tutorial-40
    Defining a Class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -12,31 +12,8 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            //tutorial-39
-            decimal num1, num2, result;
-            try
-            {
-                Console.Write("Num1: ");
-                num1 = Convert.ToDecimal(Console.ReadLine());
-                Console.Write("Num2: ");
-                num2 = Convert.ToDecimal(Console.ReadLine());
-                result = num1 / num2;
-                Console.WriteLine($"{num1} / {num2} == {result}");
-            }
-            catch(FormatException e)
-            {
-                Console.WriteLine("Invalid Input");
-                Console.WriteLine(e.Message);
-            }
-            catch(DivideByZeroException e)
-            {
-                Console.WriteLine("Can not divide by zero.");
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                Console.WriteLine("Have a nice day!");
-            }
+            //tutorial-40
+            
             Console.ReadLine(); // to make cmd stay on-screen
         }
            
